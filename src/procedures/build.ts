36d24388fa25748cce189b4c7a0ed7087f16d421
Richard Ayotte <rich@ayottesoftware.com>
2024-03-27 18:59:05 -0400

    refactor: move service files creation to build command

--- src/procedures/build.ts
+++ src/procedures/build.ts
@@ -1,12 +1,17 @@
 import fs from 'node:fs'
 import { mkdir } from 'node:fs/promises'
 import path from 'node:path'
-import { $ } from 'bun'
+import ini from '@richayotte/ini'
+import { $, write } from 'bun'
 import { get_binary_version_stdouts } from '~/data/get_binary_version_stdouts'
 import { get_files } from '~/data/get_files'
+import type { ServiceName } from '~/data/systemd/get_service_file'
+import { get_service_file } from '~/data/systemd/get_service_file'
+import { validate_systemd_service } from '~/flow/validators/systemd_service'
 import type { Procedure } from '~/procedures/types'
 import type { ProcedureOptions } from '~/tezonator'
 import { get_binary_version } from '~/transformers/get_binary_version'
+import { get_config_dir } from '~/transformers/get_config_dir'
 import { get_filtered_obj } from '~/transformers/get_filtered_object'
 import { get_sexp_object } from '~/transformers/get_sexp_object'
 
@@ -239,6 +244,117 @@ const install_binaries: Procedure<ProcedureOptions> = {
 	dependencies: [make_binaries],
 }
 
+const create_service_files: Procedure<ProcedureOptions> = {
+	id: Symbol('create service files'),
+	run: async options => {
+		const node_data_dir = get_config_dir({
+			procedure_options: options,
+			type: 'node',
+		})
+		const config_file_path = path.join(node_data_dir, 'config.json')
+
+		const bin_dir = path.join(
+			options.user_paths.bin,
+			options.tezos_network.git_ref,
+		)
+
+		const service_names: ServiceName[] = ['node', 'baker', 'accuser']
+
+		const service_file_names = service_names.map(
+			service_name =>
+				`octez-${service_name}-${options.tezos_network.human_name.toLowerCase()}.service`,
+		)
+
+		const [
+			octez_node_service_filename,
+			octez_baker_service_filename,
+			octez_accuser_service_filename,
+		] = service_file_names
+
+		const [node_service, baker_service, accuser_service] = await Promise.all(
+			service_names.map(async service_name => {
+				const service = ini.parse(await get_service_file({ service_name }))
+				if (validate_systemd_service(service)) {
+					return service
+				}
+				console.error(validate_systemd_service.errors)
+			}),
+		)
+
+		if (!(node_service && baker_service && accuser_service)) {
+			throw new Error('invalid service files')
+		}
+
+		node_service.Unit.Description = `Octez Node - ${options.tezos_network.human_name}`
+		node_service.Service.ExecStart = [
+			path.join(bin_dir, 'octez-node'),
+			'run',
+			['--config-file', config_file_path].join('='),
+		]
+			.join(' ')
+			.trim()
+		node_service.Install.RequiredBy = [
+			octez_baker_service_filename,
+			octez_accuser_service_filename,
+		].join(' ')
+
+		baker_service.Unit.Description = `Octez Baker - ${options.tezos_network.human_name}`
+		baker_service.Service.ExecStart = [
+			path.join(
+				bin_dir,
+				`octez-baker-${options.tezos_network.last_baking_daemon}`,
+			),
+			'run',
+			['--config-file', config_file_path].join('='),
+		]
+			.join(' ')
+			.trim()
+		baker_service.Unit.After = octez_node_service_filename
+		baker_service.Unit.BindsTo = octez_node_service_filename
+		baker_service.Install.WantedBy = octez_node_service_filename
+
+		accuser_service.Unit.Description = `Octez Accuser - ${options.tezos_network.human_name}`
+		accuser_service.Service.ExecStart = [
+			path.join(
+				bin_dir,
+				`octez-accuser-${options.tezos_network.last_baking_daemon}`,
+			),
+			'run',
+			['--config-file', config_file_path].join('='),
+		]
+			.join(' ')
+			.trim()
+		accuser_service.Unit.After = octez_node_service_filename
+		accuser_service.Unit.BindsTo = octez_node_service_filename
+		accuser_service.Install.WantedBy = octez_node_service_filename
+
+		// const service_file
+		await Promise.all([
+			write(
+				path.join(options.user_paths.systemd, octez_node_service_filename),
+				ini.stringify(node_service, {}),
+			),
+			write(
+				path.join(options.user_paths.systemd, octez_baker_service_filename),
+				ini.stringify(baker_service, {}),
+			),
+			write(
+				path.join(options.user_paths.systemd, octez_accuser_service_filename),
+				ini.stringify(accuser_service, {}),
+			),
+		])
+
+		await Promise.all(
+			service_file_names.map(service_file_name =>
+				$`systemctl --user enable ${service_file_name}`.quiet(),
+			),
+		)
+
+		await $`systemctl --user daemon-reload`.quiet()
+	},
+	dependencies: [install_binaries],
+}
+
 export const build_procedures: Procedure<ProcedureOptions>[] = [
-	install_binaries,
+	create_service_files,
 ]
--- src/procedures/init.ts
+++ src/procedures/init.ts
@@ -1,15 +1,36 @@
+import fs from 'node:fs'
 import { mkdir } from 'node:fs/promises'
 import path from 'node:path'
-import ini from '@richayotte/ini'
-import { $, write } from 'bun'
-import { get_service_file } from '~/data/systemd/get_service_file'
-import type { ServiceName } from '~/data/systemd/get_service_file'
-import { validate_systemd_service } from '~/flow/validators/systemd_service'
+import { $ } from 'bun'
 import type { Procedure } from '~/procedures/types'
 import type { ProcedureOptions } from '~/tezonator'
 import { get_config_dir } from '~/transformers/get_config_dir'
 
 const init_client: Procedure<ProcedureOptions> = {
+	async can_skip(options) {
+		if (options.command_options.force === true) {
+			return false
+		}
+
+		const bin_dir = path.join(
+			options.user_paths.bin,
+			options.tezos_network.git_ref,
+		)
+
+		const client_data_dir = get_config_dir({
+			procedure_options: options,
+			type: 'client',
+		})
+
+		const output = await $`${path.join(
+			bin_dir,
+			'octez-client',
+		)} --base-dir ${client_data_dir} --endpoint ${
+			options.tezos_network.rpc_url
+		} config show`.quiet()
+
+		return output.exitCode === 0
+	},
 	id: Symbol('init octez client'),
 	run: async options => {
 		const client_data_dir = get_config_dir({
@@ -38,6 +59,28 @@ const init_client: Procedure<ProcedureOptions> = {
 }
 
 const init_node: Procedure<ProcedureOptions> = {
+	async can_skip(options) {
+		if (options.command_options.force === true) {
+			return false
+		}
+
+		const node_data_dir = get_config_dir({
+			procedure_options: options,
+			type: 'node',
+		})
+
+		const bin_dir = path.join(
+			options.user_paths.bin,
+			options.tezos_network.git_ref,
+		)
+
+		const output = await $`${path.join(
+			bin_dir,
+			'octez-node',
+		)} config show --data-dir ${node_data_dir}`.quiet()
+
+		return output.exitCode === 0
+	},
 	id: Symbol('init octez node'),
 	run: async options => {
 		const node_data_dir = get_config_dir({
@@ -56,6 +99,18 @@ const init_node: Procedure<ProcedureOptions> = {
 
 		await mkdir(node_data_dir, { recursive: true })
 
+		const config_show_output = await $`${path.join(
+			bin_dir,
+			'octez-node',
+		)} config show --data-dir ${node_data_dir}`.quiet()
+
+		if (config_show_output.exitCode !== 0) {
+			// config is invalid. Move it to a backup.
+			await $`mv config.json config-${Date.now()}.json`
+				.cwd(node_data_dir)
+				.quiet()
+		}
+
 		const output = await $`${path.join(
 			bin_dir,
 			'octez-node',
@@ -68,6 +123,18 @@ const init_node: Procedure<ProcedureOptions> = {
 }
 
 const generate_identity: Procedure<ProcedureOptions> = {
+	async can_skip(options) {
+		if (options.command_options.force === true) {
+			return false
+		}
+
+		const node_data_dir = get_config_dir({
+			procedure_options: options,
+			type: 'node',
+		})
+
+		return fs.existsSync(path.join(node_data_dir, 'identity.json'))
+	},
 	id: Symbol('generate identity'),
 	run: async options => {
 		const node_data_dir = get_config_dir({
@@ -82,6 +149,8 @@ const generate_identity: Procedure<ProcedureOptions> = {
 
 		const config_file = path.join(node_data_dir, 'config.json')
 
+		await mkdir(node_data_dir, { recursive: true })
+
 		const output = await $`${path.join(
 			bin_dir,
 			'octez-node',
@@ -91,110 +160,10 @@ const generate_identity: Procedure<ProcedureOptions> = {
 			throw new Error(output.stderr.toString())
 		}
 	},
-	dependencies: [init_node],
-}
-
-const create_service_files: Procedure<ProcedureOptions> = {
-	id: Symbol('create service files'),
-	run: async options => {
-		const node_data_dir = get_config_dir({
-			procedure_options: options,
-			type: 'node',
-		})
-		const config_file_path = path.join(node_data_dir, 'config.json')
-
-		const bin_dir = path.join(
-			options.user_paths.bin,
-			options.tezos_network.git_ref,
-		)
-
-		const service_names: ServiceName[] = ['node', 'baker', 'accuser']
-
-		const [
-			octez_node_service_filename,
-			octez_baker_service_filename,
-			octez_accuser_service_filename,
-		] = service_names.map(
-			service_name =>
-				`octez-${service_name}-${options.tezos_network.human_name.toLowerCase()}.service`,
-		)
-
-		const [node_service, baker_service, accuser_service] = await Promise.all(
-			service_names.map(async service_name => {
-				const service = ini.parse(await get_service_file({ service_name }))
-				if (validate_systemd_service(service)) {
-					return service
-				}
-				console.error(validate_systemd_service.errors)
-			}),
-		)
-
-		if (!(node_service && baker_service && accuser_service)) {
-			throw new Error('invalid service files')
-		}
-
-		node_service.Unit.Description = `Octez Node - ${options.tezos_network.human_name}`
-		node_service.Service.ExecStart = [
-			path.join(bin_dir, 'octez-node'),
-			'run',
-			['--config-file', config_file_path].join('='),
-		]
-			.join(' ')
-			.trim()
-		node_service.Install.RequiredBy = [
-			octez_baker_service_filename,
-			octez_accuser_service_filename,
-		].join(' ')
-
-		baker_service.Unit.Description = `Octez Baker - ${options.tezos_network.human_name}`
-		baker_service.Service.ExecStart = [
-			path.join(
-				bin_dir,
-				`octez-baker-${options.tezos_network.last_baking_daemon}`,
-			),
-			'run',
-			['--config-file', config_file_path].join('='),
-		]
-			.join(' ')
-			.trim()
-		baker_service.Unit.After = octez_node_service_filename
-		baker_service.Unit.BindsTo = octez_node_service_filename
-		baker_service.Install.WantedBy = octez_node_service_filename
-
-		accuser_service.Unit.Description = `Octez Accuser - ${options.tezos_network.human_name}`
-		accuser_service.Service.ExecStart = [
-			path.join(
-				bin_dir,
-				`octez-accuser-${options.tezos_network.last_baking_daemon}`,
-			),
-			'run',
-			['--config-file', config_file_path].join('='),
-		]
-			.join(' ')
-			.trim()
-		accuser_service.Unit.After = octez_node_service_filename
-		accuser_service.Unit.BindsTo = octez_node_service_filename
-		accuser_service.Install.WantedBy = octez_node_service_filename
-
-		await Promise.all([
-			write(
-				path.join(options.user_paths.systemd, octez_node_service_filename),
-				ini.stringify(node_service, {}),
-			),
-			write(
-				path.join(options.user_paths.systemd, octez_baker_service_filename),
-				ini.stringify(baker_service, {}),
-			),
-			write(
-				path.join(options.user_paths.systemd, octez_accuser_service_filename),
-				ini.stringify(accuser_service, {}),
-			),
-		])
-	},
 }
 
 export const init_procedures: Procedure<ProcedureOptions>[] = [
 	init_client,
+	init_node,
 	generate_identity,
-	create_service_files,
 ]
