97c9be31e4bb62a65dba54510e58ec32743a0611
Richard Ayotte <rich@ayottesoftware.com>
2024-04-03 17:31:37 -0400

    chore: lint

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,4 +1,5 @@
 import { expect } from 'bun:test'
 import { toBeValidSchemaData } from '~/tests/matchers/to_be_valid_schema_data'
 
+// biome-ignore lint/nursery/noMisplacedAssertion: not a valid instance
 expect.extend({ toBeValidSchemaData })
